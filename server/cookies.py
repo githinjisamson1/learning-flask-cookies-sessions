1b8373ce0e8dabd7055c2b14c2e6f6276b14b93f
githinjisamson1 <githinjisamson148@gmail.com>
2024-01-16 08:53:44 +0300

    Understand cookies

--- server/cookies.py
+++ server/cookies.py
@@ -2,11 +2,13 @@ from flask import Flask, make_response, request
 
 app = Flask(__name__)
 
+# read a cookie
 @app.route("/")
 def home():
     theme = request.cookies.get("theme")
     return f"Theme is {theme}"
 
+# set a cookie
 @app.route("/cookie")
 def cookie():
     response = make_response("<h1>Cookie set</h1>")
@@ -14,13 +16,13 @@ def cookie():
     return response
 
 if __name__ == "__main__":
-    app.run(port=5555, debig=True)
+    app.run(port=5555, debug=True)
     
     
 # !data is stored in browser => insecure
 # !Cookie is in text format, user can modify
 # !hence cannot store sensitive information using cookies
 # browser makes request
-# server sends response with Set-Cookie in response headers
+# server sends response with Set-Cookie: theme=dark in response headers
 # browser makes subsequent request with Cookie attached
-# server looks up the Cookie hence persist previous context
+# server looks up the Cookie: theme=dark hence persist previous context
